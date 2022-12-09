63ba153a242926f73888945c3c6ffecc702d06a5
Mark Mead <itsmarkmead@gmail.com>
2022-12-09 14:29:41 +0000

    Update quotes

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,11 @@
 buildPlugin({
-  entryPoints: [`builds/cdn.js`],
+  entryPoints: ['builds/cdn.js'],
   outfile: 'dist/cursor.min.js',
 })
 
 buildPlugin({
-  entryPoints: [`builds/module.js`],
-  outfile: `dist/cursor.esm.js`,
+  entryPoints: ['builds/module.js'],
+  outfile: 'dist/cursor.esm.js',
   platform: 'neutral',
   mainFields: ['main', 'module'],
 })
