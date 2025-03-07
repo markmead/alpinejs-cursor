80259c77860e64237edd1e17b7e6ea74b27824d7
Mark Mead <itsmarkmead@gmail.com>
2025-03-07 13:34:15 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import Cursor from 'custom-cursor'
 
 export default function (Alpine) {
   Alpine.directive('cursor', (el, { modifiers }) => {
-    const cursorCount = Number(modifiers[0]) ?? 0
+    const [cursorCount] = modifiers
+
     const hoverTargets = JSON.parse(el.getAttribute('x-cursor-targets'))
 
     new Cursor({
-      count: cursorCount,
+      count: Number(cursorCount) || 0,
       targets: hoverTargets,
     })
   })
